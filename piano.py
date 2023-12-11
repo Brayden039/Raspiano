8a61fbab04e17c83084447a152e3a3b54e4c0f9d
Vishal Mehta <96319819+VishalMehta06@users.noreply.github.com>
2023-12-11 10:16:26 -0500

    Bug Fixes

--- piano.py
+++ piano.py
@@ -20,25 +20,17 @@ sound_8 = pygame.mixer.Sound("mixkit-atm-cash-machine-key-press-2841.wav")
 sound_9 = pygame.mixer.Sound("mixkit-atm-cash-machine-key-press-2841.wav")
 sound_10 = pygame.mixer.Sound("mixkit-atm-cash-machine-key-press-2841.wav")
 
-# Define the key-to-sound mapping
-key_sound_mapping = {
-     pygame.K_1: (sound_1, channels[0]),
-    pygame.K_2: (sound_2, channels[1]),
-    pygame.K_3: (sound_3, channels[2]),
-    pygame.K_4: (sound_4, channels[3]),
-    pygame.K_5: (sound_5, channels[4]),
-    pygame.K_6: (sound_6, channels[5]),
-    pygame.K_7: (sound_7, channels[6]),
-    pygame.K_8: (sound_8, channels[7]),
-    pygame.K_9: (sound_9, channels[8]),
-    pygame.K_0: (sound_10, channels[9]),
-    # Add more key mappings as needed
-}
-
 # Set up mixer channels
 channels = []  # Adjust based on your needs
 for i in range(2):
         channels.append(pygame.mixer.Channel(i))
+        
+# Define the key-to-sound mapping
+key_sound_mapping = {
+	pygame.K_1: (sound_1, channels[0]),
+	pygame.K_2: (sound_2, channels[1]),
+	# Add more key mappings as needed
+}
 
 # Set up the display
 pygame.display.set_caption("Raspiano Music")
@@ -53,7 +45,7 @@ while running:
         elif event.type == pygame.KEYDOWN:
             # Check if the pressed key has a sound mapping
             if event.key in key_sound_mapping:
-                sound_to_play = key_sound_mapping[event.key]
+                sound_to_play = key_sound_mapping[event.key][0]
 
                 # Play the sound on all available channels with no delay
                 for channel in channels:
